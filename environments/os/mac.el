4bb6ff216cd2d24cbf47dab3264fc4c1ae365f24
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-04-25 23:50:38 +0900

    add migemo

--- environments/os/mac.el
+++ environments/os/mac.el
@@ -36,3 +36,15 @@
   (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
   (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
   (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
+
+;; Migemo
+(setq load-path (cons "~/.emacs.d/elisp/" load-path))
+(require 'migemo)
+(setq migemo-command "/usr/local/bin/cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(setq migemo-regex-dictionary nil)
+(load-library "migemo")
+(migemo-init)
