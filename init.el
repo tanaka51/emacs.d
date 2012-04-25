f048f7301c2a94aebd8f800a10f83e4e22326c3d
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-04-26 01:05:29 +0900

    add direx

--- init.el
+++ init.el
@@ -97,3 +97,15 @@
 ;; popwin
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
+
+;; direx
+(require 'direx)
+;; direx:direx-modeのバッファをウィンドウ左辺に幅25でポップアップ
+;; :dedicatedにtを指定することで、direxウィンドウ内でのバッファの切り替えが
+;; ポップアップ前のウィンドウに移譲される
+(push '(direx:direx-mode :position left :width 25 :dedicated t)
+      popwin:special-display-config)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
+(setq direx:leaf-icon "  "
+      direx:open-icon "↓"
+      direx:closed-icon "→")
