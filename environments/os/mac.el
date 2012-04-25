fdad9321e1ebc1beda7e5e0f323a430b73a209c9
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-04-25 23:39:54 +0900

    set up window for mac

--- environments/os/mac.el
+++ environments/os/mac.el
@@ -8,3 +8,31 @@
 ;; mac では menu-bar-mode と同じ動作っぽく、再び menu-bar が現れる
 ;; (tool-bar-mode nil)
 (menu-bar-mode nil)
+
+;; window サイズ設定
+(if window-system
+    (progn
+      (set-frame-parameter nil 'alpha 90) ; 透明度
+      (setq line-spacing 0.2) ; 行間
+      (setq ns-pop-up-frames nil)
+      (setq initial-frame-alist
+          (append (list
+                   '(width . 175)
+                   '(height . 69)
+                   '(top . 0)
+                   '(left . 0))
+                  initial-frame-alist))))
+
+;; フォント設定方法 by igaiga
+;; フレームのフォントを設定
+(let* ((size 12) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+       (asciifont "Ricty") ; ASCIIフォント
+       (jpfont "Ricty") ; 日本語フォント
+       (h (* size 12))
+       (fontspec (font-spec :family asciifont))
+       (jp-fontspec (font-spec :family jpfont)))
+  (set-face-attribute 'default nil :family asciifont :height h)
+  (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+  (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
+  (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
