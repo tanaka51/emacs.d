ea2ac50a3f511a40675281cda22f47de7e9a4ef5
Koichi Tanaka <kouichi.tanaka@ctc-g.co.jp>
2012-04-26 11:09:40 +0900

    add rhtml-mode

--- init.el
+++ init.el
@@ -132,3 +132,9 @@
 ;; Rinari
 (add-to-list 'load-path "~/.emacs.d/rinari")
 (require 'rinari)
+
+;; rhtml-mode
+(add-to-list 'load-path "~/.emacs.d/rhtml")
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+    (lambda () (rinari-launch)))
