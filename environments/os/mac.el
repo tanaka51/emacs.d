6cd2dc579c5781cb39c5ac2c02149e6799ac2e06
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-05-01 12:40:30 +0900

    add thingatpt and set up it for mac dict

--- environments/os/mac.el
+++ environments/os/mac.el
@@ -48,3 +48,15 @@
 (setq migemo-regex-dictionary nil)
 (load-library "migemo")
 (migemo-init)
+
+;; MacOS X Dict
+(require 'thingatpt)
+(defun macdict-lookup (word)
+  "Lookup word with Dictionary.app"
+  (call-process "open" nil 0 nil (concat "dict://" word)))
+
+(defun macdict-lookup-word ()
+  "Lookup the word at point with Dictionary.app."
+  (interactive)
+  (macdict-lookup (word-at-point)))
+(global-set-key (kbd "C-^") 'macdict-lookup-word)
