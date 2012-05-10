5a937f4708abfe48237d55f38425f7617b5b2258
Koichi Tanaka <kouichi.tanaka@ctc-g.co.jp>
2012-05-10 09:58:37 +0900

    add ruby-electric and ruby-block

--- init.el
+++ init.el
@@ -153,4 +153,22 @@
 (add-to-list 'load-path "~/.emacs.d/rhtml")
 (require 'rhtml-mode)
 (add-hook 'rhtml-mode-hook
-    (lambda () (rinari-launch)))
+          '(lambda () (rinari-launch)))
+
+;; ruby-electric
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook
+          (let ()
+            (defun ruby-insert-end ()
+              "Insert \"end\" at point and reindent current line."
+              (interactive)
+              (insert "end")
+              (ruby-indent-line t)
+              (end-of-line))
+            '(lambda ()
+               (ruby-electric-mode t))))
+
+;; ruby-block.el
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
