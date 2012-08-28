3739a1bb18b9f71e70ea058e4e10474fc76b1d70
Koichi Tanaka <kou11031@gmail.com>
2012-08-28 10:25:24 +0900

    Resolve confilct miss

--- environments/languages/ruby-and-rails.el
+++ environments/languages/ruby-and-rails.el
@@ -30,3 +30,9 @@
           '(lambda () (rinari-launch)))
 
 (require 'yaml-mode)
+
+;; ruby-mode
+(autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
--- init.el
+++ init.el
@@ -144,39 +144,3 @@
 
 ;; grep-edit
 (require 'grep-edit)
-
-;; TODO: プログラミング用モード設定は分割する方針で
-
-;; Rinari
-(add-to-list 'load-path "~/.emacs.d/rinari")
-(require 'rinari)
-
-;; rhtml-mode
-(add-to-list 'load-path "~/.emacs.d/rhtml")
-(require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-          '(lambda () (rinari-launch)))
-
-;; ruby-mode
-(autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
-
-;; ruby-electric
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook
-          (let ()
-            (defun ruby-insert-end ()
-              "Insert \"end\" at point and reindent current line."
-              (interactive)
-              (insert "end")
-              (ruby-indent-line t)
-              (end-of-line))
-            '(lambda ()
-               (ruby-electric-mode t))))
-
-;; ruby-block.el
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle t)
