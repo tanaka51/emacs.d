4ed33957a5ad1480c67646e26101fa02588f2926
Koichi Tanaka <kou11031@gmail.com>
2012-08-28 09:19:57 +0900

    Add grep-edit

--- init.el
+++ init.el
@@ -142,4 +142,35 @@
 (push '(egg-commit-buffer-mode :position bottom :height 34) popwin:special-display-config)
 (push '("*vc-change-log*" :position bottom :height 34) popwin:special-display-config)
 
+;; grep-edit
 (require 'grep-edit)
+
+;; TODO: プログラミング用モード設定は分割する方針で
+
+;; Rinari
+(add-to-list 'load-path "~/.emacs.d/rinari")
+(require 'rinari)
+
+;; rhtml-mode
+(add-to-list 'load-path "~/.emacs.d/rhtml")
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+          '(lambda () (rinari-launch)))
+
+;; ruby-electric
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook
+          (let ()
+            (defun ruby-insert-end ()
+              "Insert \"end\" at point and reindent current line."
+              (interactive)
+              (insert "end")
+              (ruby-indent-line t)
+              (end-of-line))
+            '(lambda ()
+               (ruby-electric-mode t))))
+
+;; ruby-block.el
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
